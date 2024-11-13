7285cdabff01da8661f37fed940a70cc7cd38af3
matheuscmsampaio <matheuschagas415@gmail.com>
2024-11-13 15:55:59 -0300

    first commit

---
+++ src/main/java/br/com/fiap/EnergiaRenovavel/EnergiaRenovavelApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.EnergiaRenovavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergiaRenovavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiaRenovavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/EnergiaRenovavel/EnergiaRenovavelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.EnergiaRenovavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiaRenovavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
